15fe99bcea3891bf472e330b93c0cee272a9d481
rbuhler <rodrigo.buhler@ymail.com>
2017-04-03 18:03:07 -0300

    HTML working fine

--- facts/views.py
+++ facts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
+from .models import NutriFact
 
 # Create your views here.
+
+
+def product_list(request):
+    products = Product.objects.all()
+    nutriFacts = NutriFact.objects.all()
+    return render(request, 'facts/product_list.html', {'products': products,
+                                                       'facts': nutriFacts})
+
