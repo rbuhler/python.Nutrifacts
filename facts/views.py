17b7c3024acd3f6ace63b6da1d6b87e4ab557145
rbuhler <rodrigo.buhler@ymail.com>
2017-04-05 16:09:40 -0300

    Query set incomplete

--- facts/views.py
+++ facts/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from .models import Product
 from .models import NutriFact
+from .models import NutriTable
 
 # Create your views here.
 
 
 def product_list(request):
     products = Product.objects.all()
-    nutriFacts = NutriFact.objects.all()
+
+    for prod in products:
+        facts = NutriFact.objects.filter(product_id=prod.id)
+        table = NutriTable.objects.filter(product_id=prod.id)
+
     return render(request, 'facts/product_list.html', {'products': products,
-                                                       'facts': nutriFacts})
+                                                       'facts': facts,
+                                                       'table': table})
 
