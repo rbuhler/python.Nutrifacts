eed63b5cba3825d31cc1e8216914e93779dafc87
rbuhler <rodrigo.buhler@ymail.com>
2017-04-27 13:35:31 -0300

    DB management - not finished

--- facts/tests/deleteDB.py
+++ facts/tests/deleteDB.py
@@ -2,7 +2,21 @@ from facts.models import Product
 from facts.models import NutriFact
 from facts.models import NutriTable
 
+print('Start >>')
 
-Product.objects.all().delete()
-NutriFact.objects.all().delete()
-NutriTable.objects.all().delete()
+if Product.objects.all().delete():
+    print('Products dropped ...')
+else:
+    print('Products NOT dropped ...')
+
+if NutriFact.objects.all().delete():
+    print('Nutrifact dropped ...')
+else:
+    print('Nutrifact NOT dropped ...')
+
+if NutriTable.objects.all().delete():
+    print('Nutritable dropped ...')
+else:
+    print('NutriTable NOT dropped ...')
+
+print('<< End')
--- facts/tests/scriptTest.py
+++ facts/tests/scriptTest.py
@@ -1,6 +1,4 @@
-from facts.models import Product
-from facts.models import NutriFact
-from facts.models import NutriTable
+from facts.models import Product, NutriTable, NutriFact
 
 products = Product.objects.all()
 nutritiontable = NutriTable.objects.all()
